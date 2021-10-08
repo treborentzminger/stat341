84b1778b3d5355b3180bd604863531136e5276ae
treborentzminger <89275272+treborentzminger@users.noreply.github.com>
2021-10-08 16:31:51 -0400

    Add files via upload

---
+++ 9:21:21.R
@@ -0,0 +1,8 @@
+#trebor entzminger 9/21/21 , calculate probabilty distrubutions 
+#since only 1/5 points are correct the chance of you getting the right point is .2 
+
+12 questions 
+each question - 5 options - 1 correct answer !!
+find the probailty of having four or less correct answers ?
+dbinom(4,size=12,prob=0.2)
+#0.1328756
