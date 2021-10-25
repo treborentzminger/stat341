85bcba834c359510c7c54d8b2e124db8cbac9cb5
treborentzminger <89275272+treborentzminger@users.noreply.github.com>
2021-10-25 15:38:34 -0400

    Add files via upload

---
+++ 10:08:21.R
@@ -0,0 +1,9 @@
+#Author: trebor entzminger , Date: Oct 06 2021, Purpose: calculate the probability #distrbute values 
+
+# calculate continouns uniform distrubution probability 
+runif(10, min=1, max=3)
+
+# find the 95th percentile of the chi squared distrubution with 7 degrees of freedom
+qchisq(.95, df=7)
+
+#14.06714
