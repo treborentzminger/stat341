b4fb947423b2523620ad82404984aad8fa5949ca
treborentzminger <89275272+treborentzminger@users.noreply.github.com>
2021-10-25 15:38:18 -0400

    Add files via upload

---
+++ 10:06:21.R
@@ -0,0 +1,4 @@
+#Author: trebor entzminger , Date: Oct 06 2021, Purpose: calculate the probability #distrbute values 
+
+# calculate continouns uniform distrubution probability 
+runif(10, min=1, max=3)
